d10d079c54d491bf2f93deb3d903ffbe44b6495d
Mahak Jain <mahakjain421@gmail.com>
2024-08-21 15:06:18 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import SearchFilter from './components/SearchFilter';
+import House from './components/House';
+import SignUp from './components/SignUp';
+import { useEffect, useState } from 'react';
+import SearchResults from './components/SearchResults';
+import { Route, Routes } from 'react-router-dom';
+import Login from './components/Login';
+import SearchedHouse from './components/SearchedHouse';
+import axios from 'axios';
+import EnquiriesList from './components/EnquiriesList';
+
+
+function App() {
+
+
+ let [housesArray,setHousesArray] =  useState([]);
+
+
+useEffect( () =>{
+      let fetchData = async () => {
+let response  = await axios.get('http://localhost:4000/houses');
+        let housesData = await response.data  ;
+        // console.log(housesData);
+        setHousesArray(housesData)
+      }
+      fetchData();
+    },[]);
+
+
+  return (
+    <div className='container-fluid'>
+        <Header/>
+        <SearchFilter housesData = {housesArray}/>
+        <Routes>
+          <Route path="/" element={<House houseInfo = {housesArray[Math.floor(Math.random() * 10)]} />} />
+          <Route path="/searchresults/:county" element={<SearchResults housesData = {housesArray}/>} />
+          <Route path="/login" element={<Login/>} />
+          <Route path="/signup" element={<SignUp/>} />
+          <Route path="/enquiries" element={<EnquiriesList/>} />
+          <Route path="/searchedhouse" element={<SearchedHouse housesData = {housesArray}/>} />
+         
+        </Routes>
+        <Footer/>
+    </div>
+  );
+}
+
+
+export default App;
