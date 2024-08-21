e00cf9ef9f4bbaabc44d97369090ca9f14821dd7
Mahakjain13 <89791465+Mahakjain13@users.noreply.github.com>
2024-08-21 15:13:46 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -21,7 +21,8 @@ function App() {
 
 useEffect( () =>{
       let fetchData = async () => {
-let response  = await axios.get('http://localhost:4000/houses');
+let response  = await axios.get(process.env.REACT_APP_BACKEND_URL+"houses"
+);
         let housesData = await response.data  ;
         // console.log(housesData);
         setHousesArray(housesData)
